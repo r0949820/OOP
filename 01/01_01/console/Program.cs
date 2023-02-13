5a4d22cfa3a3889743a630df877aa9c7cd8ca344
r0949820 <r0949820@student.thomasmore.be>
2023-02-13 22:43:41 +0100

    Oefening 01.01 klaar

--- 01/01_01/console/Program.cs
+++ 01/01_01/console/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using models;
 
 namespace console
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declaratie en initialisatie van object
+            Teller teller = new Teller();
+
+            //Waardes opvragen
+            Console.Write("Geef een keuze in van 0 tot 3: ");
+            int waarde = int.Parse(Console.ReadLine());
+
+            //Gegevens toekenen aan object
+            while (waarde != 0)
+            {
+                if (waarde == 1)
+                {
+                    teller.Verhoog();
+                }
+                else if (waarde == 2)
+                {
+                    teller.Verlaag();
+                }
+                else if (waarde == 3)
+                {
+                    teller.Resetten();
+                }
+                Console.Write("Geef een keuze in van 0 tot 3: ");
+                waarde = int.Parse(Console.ReadLine()) ;
+            }
+
+           
+            Console.WriteLine(teller.ToonGegevens());
+
+
         }
     }
 }
---
+++ 01/01_01/models/Teller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace models
+{
+    public class Teller
+    {
+        //Deel 1: Atributen
+        private int _waarde;
+
+        //Deel 2: Properties
+        public int Waarde
+        {
+            get { return _waarde; }
+            set { _waarde = value; }
+        }
+
+        //Deel 3: Constructors
+        public Teller()
+        {
+            Waarde = 0;
+        }
+
+        //Deel 4: Methodes
+        public void Verhoog()
+        {
+            Waarde++;
+        }
+
+        public void Verlaag()
+        {
+            Waarde--;
+        }
+
+        public void Resetten()
+        {
+            Waarde = 0;
+        }
+
+        public string ToonGegevens()
+        {
+            return $"{Waarde}";
+        }
+    }
+}

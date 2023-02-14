5b91b83e667b8ddd5fc984753031d7f39d629b30
r0949820 <r0949820@student.thomasmore.be>
2023-02-14 17:32:34 +0100

    Oefening 01.02 klaar

--- 01/01_02/console/Program.cs
+++ 01/01_02/console/Program.cs
@@ -1,12 +1,61 @@
-﻿using System;
+﻿using models;
+using System;
 
 namespace console
 {
-    class Program
+   internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Televisie televisie = new Televisie();
+            int kanaal, volume, keuze = 0;
+
+            do
+            {
+                Console.Write("Geef een kanaal tussen 1 & 30: ");
+                kanaal = int.Parse(Console.ReadLine());
+            } while (kanaal < 1 || kanaal > 30);
+            
+            
+            televisie.Kanaal = kanaal;
+
+            do
+            {
+                Console.Write("Geef een het volume tussen 0 & 10: ");
+                volume = int.Parse(Console.ReadLine());
+            } while (volume < 0 || volume > 10);
+            
+
+            televisie.Volume= volume;
+
+            Console.WriteLine("1: Kanaal verhogen \n2: Kanaal verlagen \n3: Volume verhogen \n4: Volume verlagen \n5: Stoppen");
+            keuze= int.Parse(Console.ReadLine());
+
+            while (keuze != 5)
+            {
+                if (keuze == 1)
+                {
+                    televisie.VermeerderKanaal();
+                }
+                else if (keuze == 2)
+                {
+                    televisie.VerminderKanaal();
+                }
+                else if (keuze == 3)
+                {
+                    televisie.VermeerderVolume();
+                }
+                else if (keuze == 4)
+                {
+                    televisie.VerminderVolume();
+                }
+
+                Console.WriteLine("1: Kanaal verhogen \n2: Kanaal verlagen \n3: Volume verhogen \n4: Volume verlagen \n5: Stoppen");
+                keuze = int.Parse(Console.ReadLine());
+            }
+           
+                Console.WriteLine(televisie.ToonGegevens());
+            
         }
     }
 }
---
+++ 01/01_02/models/Televisie.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace models
+{
+    public class Televisie
+    {
+        //Deel 1: Atributen
+        private int _kanaal;
+        private int _volume;
+
+        //Deel 2: Properties
+        public int Kanaal
+        {
+            get { return _kanaal; }
+            set {
+                if (value < 1)
+                {
+                    _kanaal = 1;
+                }
+                if (value > 30)
+                {
+                    _kanaal = 30;
+                }
+                else
+                {
+                    _kanaal = value;
+                }
+            }
+        }
+        public int Volume
+        {
+            get { return _volume; }
+            set
+            {
+                if (value < 0)
+                {
+                    _volume = 0;
+                }
+                if (value > 10)
+                {
+                    _volume = 10;
+                }
+                else
+                {
+                    _volume = value;
+                }
+            }
+        }
+
+        //Deel 3: Constructors
+        public Televisie()
+        {
+            Kanaal= 0;
+            Volume= 0;
+        }
+
+        //Deel 4: Methodes
+        
+        public void VermeerderKanaal()
+        {
+            Kanaal++;
+        }
+        public void VerminderKanaal()
+        {
+            Kanaal--;
+        }
+        public void VermeerderVolume()
+        {
+            Volume++;
+        }
+        public void VerminderVolume()
+        {
+            Volume--;
+        }
+        public string ToonGegevens()
+        {
+            return $"\nKanaal: {Kanaal}\nVolume: {Volume}";
+        }
+    }
+}
